cfeda6dabda1b332c732ff7bf311a5e6d8ed438d
Bogdans <b.ozerkins@gmail.com>
2018-03-19 21:52:59 +0200

    ids autoincrement for tables

---
+++ database/instructions/ids.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'location' => '/vagrant/database/migrations/../../data/ids',
+    'structure' => array (
+  0 => 
+  array (
+    'id' => 1,
+    'name' => 'table',
+    'type' => 3,
+    'size' => 100,
+  ),
+  1 => 
+  array (
+    'id' => 2,
+    'name' => 'last_id',
+    'type' => 1,
+    'size' => 4,
+  ),
+)
+];
